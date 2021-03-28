0b72307288684ba22f2b1eda6df457c742043c02
Hammy <sgoudham@gmail.com>
2021-03-28 06:36:22 +0100

    Refactor Code

--- src/main/java/AllCustomers.java
+++ src/main/java/AllCustomers.java
@@ -1,29 +1,51 @@
+import exception.FileNotReadException;
+import exception.MyFileWriterException;
+import util.Customer;
+import util.CustomerSource;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class AllCustomers {
-    private List<Customer> listOfCustomers = new ArrayList<Customer>();
-    private boolean customersLoaded = false;
 
-    private List<String[]> loadRawCustomers(){
-        ReadDelimitedFile readDelimitedFile = new ReadDelimitedFile();
-        return readDelimitedFile.getFileData("customer.csv");
+    private List<Customer> customerList = new ArrayList<>();
+
+    private CustomerSource customerSource;
+
+    public AllCustomers() {
+        customerSource = new CustomerSource("customer.csv");
     }
 
-    private List<Customer>  loadAsCustomers(){
-        List<String[]> rawCustomers = loadRawCustomers();
-        for (String[] rawCustomer : rawCustomers) {
-            listOfCustomers.add( new Customer(rawCustomer[0],rawCustomer[1],rawCustomer[2],rawCustomer[3]));
+    public void populateCustomers() throws FileNotReadException {
+        customerSource.populateCustomers(customerList);
+    }
+
+    public void insertCustomer(String customerEmailAddress, String customerForename, String customerSurname, String customerPassword) throws MyFileWriterException {
+        customerSource.insertCustomer(customerEmailAddress, customerForename, customerSurname, customerPassword);
+    }
+
+    public Customer getSingleCustomer(String customerEmailAddress) {
+        Customer customer = null;
+
+        for (Customer currentCustomer : customerList) {
+            if (currentCustomer.getEmailAddress().equals(customerEmailAddress)) {
+                customer = currentCustomer;
+                break;
+            }
         }
-        customersLoaded = true;
-        return listOfCustomers;
-    }
-    public List<Customer>  getListOfCustomers() {
-        if (! customersLoaded){
-            loadAsCustomers();
-        }
-        return listOfCustomers;
+
+        return customer;
     }
 
+    public void setCustomerList(List<Customer> customerList) {
+        this.customerList = customerList;
+    }
 
+    public void setCustomerSource(CustomerSource customerSource) {
+        this.customerSource = customerSource;
+    }
+
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
 }
